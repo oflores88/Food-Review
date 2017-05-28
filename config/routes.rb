3038e6d1e1abdf02fc974ef780cf75af36d46231
Oscar <oscarnoe1988@gmail.com>
2017-05-27 21:04:40 -0400

    root directory

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :restaurants
   resources :users
+  root "static#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
