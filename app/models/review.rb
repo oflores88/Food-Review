1869467fec741a7fab88e1c0aa4f97197d3c9d2d
Shannan Muench <shannan.muench@gmail.com>
2017-05-29 01:53:15 -0400

    updated links on pages, corrected headings, added average scores

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  # I have no idea if we need to scope this here or not
   scope :ordered, -> {includes(:reviews).order('reviews.score')}
 end
