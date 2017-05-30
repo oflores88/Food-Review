1bc9766d26b48cb2c96f86f7164998483f26f530
Shannan Muench <shannan.muench@gmail.com>
2017-05-30 17:58:08 -0400

    fixed links on add review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,10 +26,9 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
-
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @review.restaurant, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -43,7 +42,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @restaurant, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
@@ -73,6 +72,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:score, :review, :user_id, :restaurant_id)
+      params.require(:review).permit(:score, :review, :user_id, :user_name, :restaurant_id, :restaurant_name)
     end
 end
